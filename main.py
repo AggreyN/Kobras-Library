7d6273aa13e1f424ef39e3e0c4afcbebb53c5a4c
AggreyN <anarh1@terpmail.umd.edu>
2025-12-23 14:58:28 -0500

    adding main code

---
+++ main.py
@@ -0,0 +1,13 @@
+import os # lets me interact with the operating system
+import mutagen #used for music file metadata
+import pandas as pd # type: ignore # used for data manipulation and analysis
+import numpy as np # type: ignore
+import sys 
+
+import importlib # used to import modules in runtime
+
+
+musicexample = os.path.join(os.getcwd(), "MusicExamples")
+print(musicexample)
+
+
